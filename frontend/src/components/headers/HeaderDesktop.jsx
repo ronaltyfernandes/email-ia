d3797add9b4671b3cd1047df0d062351ad344fd0
ronalty fernandes <ronaltyfernandess@gmail.com>
2026-07-12 16:23:53 -0300

    modificando aparencia e colocando loading de chamada da da api

--- frontend/src/components/headers/HeaderDesktop.jsx
+++ frontend/src/components/headers/HeaderDesktop.jsx
@@ -17,11 +17,23 @@ export function HeaderDesktop() {
         shadow-black
       "
     >
-      <div className="sombra-azul">▲utoU</div>
-      <nav className="">
-        <NavLinkHeader end to="/" text="Home" />
-        <NavLinkHeader to="/respostas" text="Respostas" />
-      </nav>
+
+      {/* Logo */}
+      <div className="flex items-center gap-2 select-none">
+        <div className="flex h-11 w-11 items-center justify-center rounded-xl bg-linear-to-br from-blue-600 to-cyan-500 text-lg font-bold text-white shadow-lg">
+          AI
+        </div>
+
+        <div className="flex flex-col leading-none">
+          <span className="text-xl font-extrabold tracking-tight text-text">
+            Email
+            <span className="text-blue-500">IA</span>
+          </span>
+          <span className="text-xs tracking-[0.3em] text-text-secondary uppercase">
+            Assistant
+          </span>
+        </div>
+      </div>
 
       <ThemeSwitch
         toggleTheme={toggleTheme}
